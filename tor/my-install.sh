b60d198b6e1c8503c47da865794d6e106aa6d795
Matt Hayden <hayden+github@morp.holo.gy>
2016-05-13 20:02:03 +0000

    tor support

--- tor/my-install.sh
+++ tor/my-install.sh
@@ -1,16 +1,20 @@
-DESTDIR=/tmp/tor-0.2.7.6
+: ${DESTDIR=/tmp/tor-0.2.7.6}
 
 for pd in /var/lib/tor /var/tmp/tor
 do
-	[[ -d "$pd" ]] || mkdir -m 0770 $DESTDIR/"$pd"
+	[[ $DESTDIR ]] && pd="$DESTDIR/$pd"
+	[[ -d "$pd" ]] || mkdir -p -m 0770 "$pd"
 	sudo chown :_tor "$pd"
 done
 for wd in /var/log/tor /var/run/tor
 do
-	[[ -d "$wd" ]] || mkdir -m 0775 $DESTDIR/"$wd"
+	[[ $DESTDIR ]] && wd="$DESTDIR/$wd"
+	[[ -d "$wd" ]] || mkdir -p -m 0775 "$wd"
 	sudo chown :_tor "$wd"
 done
 
 cp contrib/dist/torctl $DESTDIR/usr/local/stow/tor/bin/
+mkdir -p $DESTDIR/etc/logrotate.d
 cp contrib/operator-tools/tor.logrotate $DESTDIR/etc/logrotate.d/tor
+mkdir -p $DESTDIR/etc/systemd
 cp contrib/dist/tor.service $DESTDIR/etc/systemd/user
