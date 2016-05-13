026fe85391736ffa6ee9dc8cd66c311a72bfe083
Matt Hayden <hayden+github@morp.holo.gy>
2016-05-13 23:22:38 +0000

    Halfway through configuring tor

--- tor/my-install.sh
+++ tor/my-install.sh
@@ -2,19 +2,19 @@
 
 for pd in /var/lib/tor /var/tmp/tor
 do
-	[[ $DESTDIR ]] && pd="$DESTDIR/$pd"
 	[[ -d "$pd" ]] || mkdir -p -m 0770 "$pd"
 	sudo chown :_tor "$pd"
 done
 for wd in /var/log/tor /var/run/tor
 do
-	[[ $DESTDIR ]] && wd="$DESTDIR/$wd"
 	[[ -d "$wd" ]] || mkdir -p -m 0775 "$wd"
 	sudo chown :_tor "$wd"
 done
 
+
+cp contrib/operator-tools/tor-exit-notice.html $DESTDIR/usr/local/stow/tor/etc/tor
 cp contrib/dist/torctl $DESTDIR/usr/local/stow/tor/bin/
 mkdir -p $DESTDIR/etc/logrotate.d
 cp contrib/operator-tools/tor.logrotate $DESTDIR/etc/logrotate.d/tor
-mkdir -p $DESTDIR/etc/systemd
+mkdir -p $DESTDIR/etc/systemd/user
 cp contrib/dist/tor.service $DESTDIR/etc/systemd/user
