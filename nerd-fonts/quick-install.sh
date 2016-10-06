c0092e8f6aa1ae8e073af8f0b01682dc18337d1b
Matt Hayden (Valenceo LTD) <hayden+git@morp.holo.gy>
2016-10-05 21:36:17 -0600

    No Makefile necessary

--- nerd-fonts/quick-install.sh
+++ nerd-fonts/quick-install.sh
@@ -1,18 +1,11 @@
 #! /usr/bin/env bash
 set -e
 
+SETUP_DIR="$PWD"
 FONT_DIR="$HOME/.local/share/fonts"
 
 mkdir -p "$FONT_DIR"
 cd "$FONT_DIR"
-wget -i - << EOF
-https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/FantasqueSansMono/Bold/complete/Fantasque%20Sans%20Mono%20Bold%20Nerd%20Font%20Complete.ttf
-https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/FantasqueSansMono/Regular/complete/Fantasque%20Sans%20Mono%20Regular%20Nerd%20Font%20Complete.ttf
-https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Inconsolata/complete/Inconsolata%20for%20Powerline%20Nerd%20Font%20Complete.otf
-https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Meslo/S/complete/Meslo%20LG%20S%20Regular%20for%20Powerline%20Nerd%20Font%20Complete.otf
-https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/RobotoMono/complete/Roboto%20Mono%20Medium%20Nerd%20Font%20Complete.ttf
-https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20derivative%20Powerline%20Nerd%20Font%20Complete.ttf
-https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/MPlus/Medium/complete/M%2B%201m%20medium%20Nerd%20Font%20Complete.ttf'
-EOF
+wget -i "$SETUP_DIR"/raw.urls
 
 fc-cache -f "$FONT_DIR"
