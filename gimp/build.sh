cbad2880c7e053464a929a3369949ea576f03d6a
Matt Hayden (Valenceo LTD.) <https://github.com/matt-hayden>
2018-05-02 21:39:04 -0600

    GIMP for Bionic Beaver

--- gimp/build.sh
+++ gimp/build.sh
@@ -1,93 +1,60 @@
 set -e
-SUDO=sudo
-MAXLOAD=$(awk "BEGIN { print $(nproc) - 0.5 }")
+[[ 0 -lt $(id -u) ]] || SUDO=sudo
+MAXLOAD=$(awk "BEGIN { print $(nproc) + 0.5 }")
 
-# Note: somehow, /usr/local/lib/pkgconfig will sometimes lose a+rX permissions.
+[[ -w /usr/local/stow ]] && PREFIX=/usr/local/stow/gimp-2.10 || PREFIX=$HOME/.local
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig
 
-mkdir -p {libpng,gexiv2,Little-CMS,libmypaint,babl,libwebp,gegl,gimp}-build
-
-if ! ldconfig -p | grep -iq libpng16.so
-then
-	cd libpng-build
-	../libpng-1.6.34/configure --prefix=/usr/local/stow/gimp-2.9
-	make -j -l $MAXLOAD install
-	cd ..
-	read -p "Done with libpng, you may need to run stow and/or ldconfig"
-fi
+mkdir -p {libmypaint,mypaint-brushes,babl,libwebp,gegl,gimp}-build
 
 
-if ! ldconfig -p | grep -iq libgexiv2.so
-then
-	cd gexiv2-build
-	../gexiv2-0.10.8/configure --prefix=/usr/local/stow/gimp-2.9 \
-		--enable-introspection
-	make -j -l $MAXLOAD
-	# Python packages
-	$SUDO make install
-	$SUDO chmod -R g+rwX /usr/local/stow/gimp-2.9
-	cd ..
-	read -p "Done with gexiv2, you may need to run stow and/or ldconfig"
-fi
-
-
-# lcms-2.9 had a misconfigured build which needed transicc_LDFLAGS = -lm
-if ! ldconfig -p | grep -iq /usr/local/lib/liblcms2.so
-then
-	cd Little-CMS
-	autoreconf -fiv
-	cd ../Little-CMS-build
-	../Little-CMS/configure --prefix=/usr/local/stow/gimp-2.9
-	make -j -l $MAXLOAD install
-	cd ..
-	read -p "Done with lcms2, you may need to run stow"
-fi
-
-if ! ldconfig -p | grep -iq libbabl-0.1.so
-then
-	cd babl-build
-	../babl-0.1.46/configure --prefix=/usr/local/stow/gimp-2.9
-	make -j -l $MAXLOAD install
-	cd ..
-	read -p "Done with babl, you may need to run stow and/or ldconfig"
-fi
-
-if ! ldconfig -p | grep -iq libmypaint-1.3.so.0
+if ! [[ -s $PREFIX/lib/libmypaint.so ]]
 then
 	cd libmypaint-build
-	../libmypaint-1.3.0/configure --prefix=/usr/local/stow/gimp-2.9
+	../libmypaint-1.3.0/configure --prefix=$PREFIX
 	make -j -l $MAXLOAD install
 	cd ..
-	read -p "Done with libmypaint, you may need to run stow and/or ldconfig"
+
+	cd mypaint-brushes-1.3.0
+	./autogen.sh
+	cd ../mypaint-brushes-build
+	../mypaint-brushes-1.3.0/configure --prefix=$PREFIX
+	make -j -l $MAXLOAD install
+	cd ..
 fi
 
-if ! ldconfig -p | grep -iq libwebp.so.7
+if ! [[ -s $PREFIX/lib/libbabl-0.1.so ]]
+then
+	cd babl-build
+	../babl-0.1.46/configure --prefix=$PREFIX
+	make -j -l $MAXLOAD install
+	cd ..
+fi
+
+if ! [[ -s $PREFIX/lib/libwebp.so ]]
 then
 	cd libwebp
 	autoreconf -fiv
 	cd ../libwebp-build
-	../libwebp/configure --enable-everything \
-		--prefix=/usr/local/stow/gimp-2.9
+	../libwebp/configure --enable-everything --prefix=$PREFIX
 	make -j -l $MAXLOAD install
 	cd ..
-	read -p "Done with libwebp, you may need to run stow and/or ldconfig"
 fi
 
-#$SUDO chmod -R a+rX /usr/local/lib/pkgconfig
+# Note: somehow, /usr/local/lib/pkgconfig will sometimes lose a+rX permissions here
 
-if ! ldconfig -p | grep -iq libgegl-0.3.so
+if ! [[ -s $PREFIX/lib/libgegl-0.4.so ]]
 then
 	cd gegl-build
-	../gegl-0.3.34/configure --prefix=/usr/local/stow/gimp-2.9
+	../gegl-0.4.0/configure --prefix=$PREFIX
 	make -j -l $MAXLOAD install
 	cd ..
-	read -p "Done with gegl, you may need to run stow and/or ldconfig"
 fi
 
-if ! type gimp-2.9
+if ! type gimp-2.10
 then
 	cd gimp-build
-	../gimp-2.9.6/configure --prefix=/usr/local/stow/gimp-2.9 \
-		--disable-vector-icons
+	../gimp-2.10.0/configure --prefix=$PREFIX
 	make -j -l $MAXLOAD install
 	cd ..
 fi
--- gimp/fetch.sh
+++ gimp/fetch.sh
@@ -1,18 +1,19 @@
 
-set -e
-SUDO=sudo
+: ${CURL='curl -sSL'}
 
+set -e
+[[ 0 -lt $(id -u) ]] && SUDO=sudo
+
+
+$CURL -o 'mypaint-brushes-1.3.0.tar.gz' 'https://github.com/Jehan/mypaint-brushes/archive/v1.3.0.tar.gz'
+$CURL	-O 'https://github.com/mypaint/libmypaint/releases/download/v1.3.0/libmypaint-1.3.0.tar.xz' \
+	-O 'https://download.gimp.org/pub/babl/0.1/babl-0.1.46.tar.bz2' \
+	-O 'https://download.gimp.org/pub/gegl/0.4/gegl-0.4.0.tar.bz2' \
+	-O 'https://download.gimp.org/mirror/pub/gimp/v2.10/gimp-2.10.0.tar.bz2' &
+
+[[ -d libwebp ]] || git clone https://chromium.googlesource.com/webm/libwebp
 
 $SUDO apt update || echo "update failed"
-xargs $SUDO apt install -y < deps.list
+sort deps.list | xargs $SUDO apt install -y
 
-curl -LO 'https://download.sourceforge.net/libpng/libpng-1.6.34.tar.xz'
-curl -LO 'https://download.gnome.org/sources/gexiv2/0.10/gexiv2-0.10.8.tar.xz'
-curl -LO 'https://github.com/mm2/Little-CMS/archive/lcms2.9.tar.gz'
-curl -LO 'https://github.com/mypaint/libmypaint/releases/download/v1.3.0/libmypaint-1.3.0.tar.xz'
-curl -LO 'https://download.gimp.org/pub/babl/0.1/babl-0.1.46.tar.bz2'
-[[ -d libwebp ]] || git clone https://chromium.googlesource.com/webm/libwebp
-curl -LO 'https://download.gimp.org/pub/gegl/0.3/gegl-0.3.34.tar.bz2'
-curl -LO 'https://download.gimp.org/mirror/pub/gimp/v2.9/gimp-2.9.6.tar.bz2'
-
-# And decompress any archives
+wait
