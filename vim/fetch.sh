d79250ebc4c4f90c395dd01eafb3595f9c4a6cb1
Matt Hayden (Valenceo LTD.) <https://github.com/matt-hayden>
2018-05-23 21:42:32 -0600

    chmod

--- vim/fetch.sh
+++ vim/fetch.sh
@@ -9,13 +9,13 @@ $CURL -O 'https://github.com/vim/vim/archive/v8.1.0022.tar.gz'
 
 $SUDO apt update || echo "update failed"
 $SUDO apt-get install -y \
-	libncurses5-dev \
-	libgnome2-dev \
-	libgnomeui-dev \
-	libgtk2.0-dev \
 	libatk1.0-dev \
 	libbonoboui2-dev \
 	libcairo2-dev \
+	libgnome2-dev \
+	libgnomeui-dev \
+	libgtk2.0-dev \
+	libncurses5-dev \
 	libx11-dev \
 	libxpm-dev \
 	libxt-dev
